15ccc3853200af7bde89f4d29b285e7f6e63f551
Dru Jensen <drujensen@gmail.com>
2016-02-26 17:31:00 -0800

    support method override
    I added a simple method override to support put and delete verbs.

--- samples/blog/config/routes.cr
+++ samples/blog/config/routes.cr
@@ -6,10 +6,10 @@ get  "/logout", SessionController::Delete.instance
 
 get  "/", PostController::Index.instance
 
-get  "/posts", PostController::Index.instance 
-get  "/posts/:id", PostController::Show.instance
-get  "/posts/new", PostController::New.instance
-post "/posts/create", PostController::Create.instance  
-get  "/posts/:id/edit", PostController::Edit.instance  
-post "/posts/:id/update", PostController::Update.instance
-get  "/posts/:id/delete", PostController::Delete.instance
+get    "/posts", PostController::Index.instance 
+get    "/posts/:id", PostController::Show.instance
+get    "/posts/new", PostController::New.instance
+post   "/posts/create", PostController::Create.instance  
+get    "/posts/:id/edit", PostController::Edit.instance  
+put    "/posts/:id/update", PostController::Update.instance
+delete "/posts/:id/delete", PostController::Delete.instance
