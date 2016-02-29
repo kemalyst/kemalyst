0fd54c784439e8ee2c97ab985a6ab8ff44baec6b
Dru Jensen <drujensen@gmail.com>
2016-02-29 14:24:41 -0800

    fixed logger for sample app

--- samples/blog/config/application.cr
+++ samples/blog/config/application.cr
@@ -1,11 +1,14 @@
 Kemalyst::Blog::Application.config do |config|
   
-  log = File("logs/development.log", "a")
-  log.write_on_flush = true
+  log = File.new("logs/development.log", "a")
+  log.flush_on_newline = true
 
   # using the built-in logger. you can create your own.
   config.logger = Logger.new(log)
   config.logger.level = Logger::DEBUG
-
+  config.logger.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
+    io << "[" << datetime << " #" << Process.pid << "] "
+    io << severity.rjust(5) << ": " << message
+  end
 
 end
