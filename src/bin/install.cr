5fd292ce011e712eb364f1010fa04a3a49bb4c1c
Dru Jensen <drujensen@gmail.com>
2016-03-04 07:43:44 -0800

    only execute if src exists

--- src/bin/install.cr
+++ src/bin/install.cr
@@ -7,19 +7,16 @@ if File.exists? src_dir
   unless File.exists? app_dir
     File.rename src_dir, app_dir
   end
-else
-  unless File.exists? app_dir
-    Dir.mkdir app_dir
-  end
-end
 
-# create structure
-dirs = ["app/controllers", "app/models", "app/views", "config", "db",
+  # create structure
+  dirs = ["app/controllers", "app/models", "app/views", "config", "db",
         "logs", "public/javascripts", "public/stylesheets"]
 
-dirs.each do |dir|
-  unless File.exists? dir
-    Dir.mkdir_p dir
+  dirs.each do |dir|
+    unless File.exists? dir
+      Dir.mkdir_p dir
+    end
   end
 end
+
 
