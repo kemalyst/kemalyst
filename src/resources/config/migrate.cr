15bc80d6ccc0d2d5cacda14ac4358d5726888584
Dru Jensen <drujensen@gmail.com>
2016-03-15 21:43:01 -0700

    remove the migration script otherwise it will run

--- src/resources/config/migrate.cr
+++
@@ -1,9 +0,0 @@
-require "../app/models/*"
-
-# Currently the migration only drops and recreates the tables.  This will lose
-# any data in these tables.  A safer approach would perform DDL SQL calls
-# using Demo.query("alter table demos set...;")
-
-Demo.drop
-Demo.create
-
