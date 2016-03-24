d771b58c2b76f91531f467e98cae5b50a360cdce
Dru Jensen <drujensen@gmail.com>
2016-03-24 14:34:26 -0700

    update chat to latest layout

---
+++ samples/chat/src/app.cr
@@ -0,0 +1,5 @@
+require "kemalyst"
+require "../config/*"
+
+Kemalyst::Application.instance.start
+
--- samples/chat/src/chat.cr
+++
@@ -1,7 +0,0 @@
-require "../../../src/kemalyst"
-require "../config/*"
-
-class Chat::Application < Kemalyst::Application
-end
-
-Chat::Application.instance.start
