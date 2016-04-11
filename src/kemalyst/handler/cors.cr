7fe0890311fa8a06a50503455580befb16201f6d
Dru Jensen <drujensen@gmail.com>
2016-04-11 15:49:04 -0700

    add other access control headers

--- src/kemalyst/handler/cors.cr
+++ src/kemalyst/handler/cors.cr
@@ -4,10 +4,10 @@ module Kemalyst::Handler
 
     def call(context)
       begin
-        context.response.headers["Access-Control-Allow-Origin"] = "*"
+        context.response.headers["access-control-allow-origin"] = "*"
         if context.request.method.downcase == "options"
-          context.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE"
-          # context.response.headers["Access-Control-Allow-Headers"] = "X-Custom-Header"
+          context.response.headers["access-control-allow-methods"] = "GET, HEAD, POST, DELETE, OPTIONS, PUT, PATCH"
+          context.response.headers["access-control-allow-headers"] = "Accept, Content-Type"
           context.response.status_code = 200
           context.response.content_type = "application/json"
         else
