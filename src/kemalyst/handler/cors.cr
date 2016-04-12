d8c4f1dd4cf8c2e495778df49889ee841388f228
Dru Jensen <drujensen@gmail.com>
2016-04-12 11:44:40 -0700

    move above OPTIONS

--- src/kemalyst/handler/cors.cr
+++ src/kemalyst/handler/cors.cr
@@ -16,12 +16,12 @@ module Kemalyst::Handler
       begin
         context.response.headers["access-control-allow-origin"] = @allow_origin
         context.response.headers["access-control-allow-headers"] = @allow_headers
+        context.response.headers["access-control-allow-methods"] = @allow_methods
         if @allow_credentials
           context.response.headers["access-control-allow-credentials"] = "true"
         end
         
         if context.request.method.downcase == "options"
-          context.response.headers["access-control-allow-methods"] = @allow_methods
           if @max_age > 0
             context.response.headers["access-control-max-age"] = @max_age.to_s
           end
