303d91f0c9d140c5465a3a4c6c0674e29aeeb948
Dru Jensen <drujensen@gmail.com>
2016-04-11 22:11:59 -0700

    try to always return allow_headers

--- src/kemalyst/handler/cors.cr
+++ src/kemalyst/handler/cors.cr
@@ -15,12 +15,12 @@ module Kemalyst::Handler
     def call(context)
       begin
         context.response.headers["access-control-allow-origin"] = @allow_origin
+        context.response.headers["access-control-allow-headers"] = @allow_headers
         if @allow_credentials
           context.response.headers["access-control-allow-credentials"] = "true"
         end
         
         if context.request.method.downcase == "options"
-          context.response.headers["access-control-allow-headers"] = @allow_headers
           context.response.headers["access-control-allow-methods"] = @allow_methods
           if @max_age > 0
             context.response.headers["access-control-max-age"] = @max_age.to_s
