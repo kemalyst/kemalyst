6a2a50dada865f18009cd7f2961775ec9ea60321
Dru Jensen <drujensen@gmail.com>
2016-04-11 21:11:34 -0700

    try only responding when requested via OPTIONS

--- src/kemalyst/handler/cors.cr
+++ src/kemalyst/handler/cors.cr
@@ -5,9 +5,9 @@ module Kemalyst::Handler
     def call(context)
       begin
         context.response.headers["access-control-allow-origin"] = "*"
-        context.response.headers["access-control-allow-headers"] = "Accept, Content-Type"
-        context.response.headers["access-control-allow-methods"] = "GET, HEAD, POST, DELETE, OPTIONS, PUT, PATCH"
         if context.request.method.downcase == "options"
+          context.response.headers["access-control-allow-headers"] = "Accept, Content-Type"
+          context.response.headers["access-control-allow-methods"] = "GET, HEAD, POST, DELETE, OPTIONS, PUT, PATCH"
           context.response.status_code = 200
           context.response.content_type = "application/json"
         else
