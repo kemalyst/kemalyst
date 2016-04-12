652a50cc5f3060e223a18bf01ff4adc9fa01559a
Dru Jensen <drujensen@gmail.com>
2016-04-11 21:19:41 -0700

    make the allow-options configurable

--- src/kemalyst/handler/cors.cr
+++ src/kemalyst/handler/cors.cr
@@ -1,13 +1,20 @@
 module Kemalyst::Handler
   # This Handler adds support for Cross Origin Resource Sharing.
   class Cors < Base
+    property allow_origin, allow_headers, allow_methods
+
+    def initialize
+      @allow_origin = "*"
+      @allow_headers = "Accept, Content-Type"
+      @allow_methods = "GET, HEAD, POST, DELETE, OPTIONS, PUT, PATCH"
+    end
 
     def call(context)
       begin
-        context.response.headers["access-control-allow-origin"] = "*"
+        context.response.headers["access-control-allow-origin"] = allow_origin
         if context.request.method.downcase == "options"
-          context.response.headers["access-control-allow-headers"] = "Accept, Content-Type"
-          context.response.headers["access-control-allow-methods"] = "GET, HEAD, POST, DELETE, OPTIONS, PUT, PATCH"
+          context.response.headers["access-control-allow-headers"] = allow_headers
+          context.response.headers["access-control-allow-methods"] = allow_methods
           context.response.status_code = 200
           context.response.content_type = "application/json"
         else
