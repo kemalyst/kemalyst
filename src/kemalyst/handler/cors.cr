48cc9d016ae11638134c5b8c28e7250095fcd336
Dru Jensen <drujensen@gmail.com>
2016-04-11 21:48:11 -0700

    try new content-type

--- src/kemalyst/handler/cors.cr
+++ src/kemalyst/handler/cors.cr
@@ -1,22 +1,33 @@
 module Kemalyst::Handler
   # This Handler adds support for Cross Origin Resource Sharing.
   class Cors < Base
-    property allow_origin, allow_headers, allow_methods
+    property allow_origin, allow_headers, allow_methods, allow_credentials,
+      max_age
 
     def initialize
       @allow_origin = "*"
       @allow_headers = "Accept, Content-Type"
       @allow_methods = "GET, HEAD, POST, DELETE, OPTIONS, PUT, PATCH"
+      @allow_credentials = false
+      @max_age = 0
     end
 
     def call(context)
       begin
-        context.response.headers["access-control-allow-origin"] = allow_origin
+        context.response.headers["access-control-allow-origin"] = @allow_origin
+        if @allow_credentials
+          context.response.headers["access-control-allow-credentials"] = true
+        end
+        
         if context.request.method.downcase == "options"
-          context.response.headers["access-control-allow-headers"] = allow_headers
-          context.response.headers["access-control-allow-methods"] = allow_methods
+          context.response.headers["access-control-allow-headers"] = @allow_headers
+          context.response.headers["access-control-allow-methods"] = @allow_methods
+          if @max_age > 0
+            context.response.headers["access-control-max-age"] = @max_age
+          end
           context.response.status_code = 200
-          context.response.content_type = "application/json"
+          context.response.content_type = "text/html; charset=utf-8"
+          context.response.print("")
         else
           call_next(context)
         end
