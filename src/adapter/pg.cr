573b02c13ac4ce70e0d3a0af44e52e4c8ae4c3e3
Dru Jensen <drujensen@gmail.com>
2016-05-31 12:36:44 -0700

    change timeout to 30 seconds

--- src/adapter/pg.cr
+++ src/adapter/pg.cr
@@ -7,7 +7,7 @@ class Kemalyst::Adapter::Pg < Kemalyst::Adapter::Base
 
   def initialize(settings)
     database = env(settings["database"].to_s)
-    @pool = ConnectionPool.new(capacity: 20) do
+    @pool = ConnectionPool.new(capacity: 20, timeout: 30.0) do
        PG.connect(database)
     end
   end
