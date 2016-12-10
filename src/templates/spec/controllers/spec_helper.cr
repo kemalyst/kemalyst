abc1328664673177c89756c8a7047d734ed542fa
Dru Jensen <drujensen@gmail.com>
2016-12-09 17:35:20 -0800

    add kemalyst-spec to template

--- src/templates/spec/controllers/spec_helper.cr
+++ src/templates/spec/controllers/spec_helper.cr
@@ -1,8 +1,2 @@
 require "../spec_helper"
-
-def create_context(request)
-  io = IO::Memory.new
-  response = HTTP::Server::Response.new(io)
-  context = HTTP::Server::Context.new(request, response)
-  return io, context
-end
+require "kemalyst-spec"
