20845f86a9e9287500cbfbd80dad810c1ba05b06
Dru Jensen <drujensen@gmail.com>
2017-05-12 22:52:40 -0700

    simplify routes and controllers

--- spec/kemalyst/controller_spec.cr
+++ spec/kemalyst/controller_spec.cr
@@ -1,9 +1,5 @@
 require "./spec_helper"
 
-ActionHelper.action TestShow do
-  "Hello World!"
-end
-
 describe Kemalyst::Controller do
   it "provides csrf token" do
     request = HTTP::Request.new("GET", "/")
@@ -19,12 +15,5 @@ describe Kemalyst::Controller do
     context.session["csrf.token"] = "test"
     controller = Kemalyst::Controller.instance
     expect(controller.csrf_tag(context)).to eq "<input type=\"hidden\" name=\"_csrf\" value=\"test\" />"
-  end
-
-  it "should create controller action with action helper" do
-    request = HTTP::Request.new("GET", "/")
-    io, context = create_context(request)
-
-    expect(TestShow.instance.call(context)).to eq "Hello World!"
   end
 end
