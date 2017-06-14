e7abe1dd6300a1247cb189e59219c9c9b7df9782
Elias Perez <eliasjpr@gmail.com>
2017-06-13 21:28:39 -0400

    Adds correct header for http method override

--- spec/kemalyst/handler/method_spec.cr
+++ spec/kemalyst/handler/method_spec.cr
@@ -1,30 +1,32 @@
 require "./spec_helper"
 
-describe Kemalyst::Handler::Method do
-  it "overrides method when params key exists" do
-    request = HTTP::Request.new("GET", "/?_method=delete")
-    io, context = create_context(request)
+module Kemalyst::Handler
+  describe Method do
+    it "overrides method when params key exists" do
+      request = HTTP::Request.new("GET", "/?#{Method::PARAMS_KEY}=delete")
+      io, context = create_context(request)
 
-    params_handler = Kemalyst::Handler::Params.instance
-    method_handler = Kemalyst::Handler::Method.instance
-    block_handler = Kemalyst::Handler::Block.new(->(c : HTTP::Server::Context) { "Hello World!" })
-    params_handler.next = method_handler
-    method_handler.next = block_handler
-    params_handler.call(context)
-    expect(request.method).to eq("delete")
-  end
+      params_handler = Kemalyst::Handler::Params.instance
+      method_handler = Kemalyst::Handler::Method.instance
+      block_handler = Kemalyst::Handler::Block.new(->(c : HTTP::Server::Context) { "Hello World!" })
+      params_handler.next = method_handler
+      method_handler.next = block_handler
+      params_handler.call(context)
+      expect(request.method).to eq("delete")
+    end
 
-  it "overrides method when header key exists" do
-    request = HTTP::Request.new("GET", "/")
-    request.headers["HTTP_X_HTTP_METHOD_OVERRIDE"] = "delete"
-    io, context = create_context(request)
+    it "overrides method when header key exists" do
+      request = HTTP::Request.new("GET", "/")
+      request.headers[Method::HEADER_KEY] = "delete"
+      io, context = create_context(request)
 
-    params_handler = Kemalyst::Handler::Params.instance
-    method_handler = Kemalyst::Handler::Method.instance
-    block_handler = Kemalyst::Handler::Block.new(->(c : HTTP::Server::Context) { "Hello World!" })
-    params_handler.next = method_handler
-    method_handler.next = block_handler
-    params_handler.call(context)
-    expect(request.method).to eq("delete")
+      params_handler = Kemalyst::Handler::Params.instance
+      method_handler = Kemalyst::Handler::Method.instance
+      block_handler = Kemalyst::Handler::Block.new(->(c : HTTP::Server::Context) { "Hello World!" })
+      params_handler.next = method_handler
+      method_handler.next = block_handler
+      params_handler.call(context)
+      expect(request.method).to eq("delete")
+    end
   end
 end
