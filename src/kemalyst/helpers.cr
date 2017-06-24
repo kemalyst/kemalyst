a8c59c4118954f5a7cb3e275980fafb67714622e
Dru Jensen <drujensen@gmail.com>
2017-06-23 21:35:31 -0700

    support legacy handlers

--- src/kemalyst/helpers.cr
+++ src/kemalyst/helpers.cr
@@ -1,35 +1,4 @@
-require "http"
-require "kilt"
-require "kilt/slang"
-require "./helpers"
-
-class Kemalyst::Controller
-  include Kemalyst::Helpers
-  property context : HTTP::Server::Context
-
-  def initialize(@context)
-  end
-
-  def request
-    context.request
-  end
-
-  def response
-    context.response
-  end
-
-  def params
-    context.params
-  end
-
-  def session
-    context.session
-  end
-
-  def flash
-    context.flash
-  end
-
+module Kemalyst::Helpers
   # Helper method to get the logger
   def logger
     Kemalyst::Application.instance.logger
@@ -45,56 +14,50 @@ class Kemalyst::Controller
     Kemalyst::Handler::CSRF.instance.tag(context)
   end
 
-  def run_before_filter(method)
-  end
-
-  def run_after_filter(method)
-  end
-
   # helper to render a view with a layout.  The view name is relative to `src/views`
   # directory and the layout is relative to `src/views/layouts` directory.
   macro render(filename, layout, *args)
-    content = render("{{filename.id}}", {{*args}})
+    content = render("{{filename.id}}", {{ *args }})
     render("layouts/{{layout.id}}")
   end
 
   # helper to render a template.  The view name is relative to `src/views` directory.
   macro render(filename, *args)
-    Kilt.render("src/views/{{filename.id}}", {{*args}})
+    Kilt.render("src/views/{{filename.id}}", {{ *args }})
   end
 
   # helper to redirect to another page.  This sets the Location header to
   # the url provided.
-  def redirect(url, status_code = 302)
-    context.response.headers.add("Location", url)
-    context.response.status_code = status_code
+  macro redirect(url, status_code = 302)
+    context.response.status_code = {{ status_code }}
+    context.response.headers.add("Location", {{ url }})
   end
 
   # helper to render text.  This sets the content_type to `plain/text`
-  def text(body, status_code = 200)
-    context.response.status_code = status_code
+  macro text(body, status_code = 200)
+    context.response.status_code = {{ status_code }}
     context.response.content_type = "text/plain"
-    context.response.print(body)
+    context.response.print({{ body }})
   end
 
   # helper to render html.  This sets the content_type to `text/html`
-  def html(body, status_code = 200)
-    context.response.status_code = status_code
+  macro html(body, status_code = 200)
+    context.response.status_code = {{ status_code }}
     context.response.content_type = "text/html; charset=UTF-8"
-    context.response.print(body)
+    context.response.print({{ body }})
   end
 
   # helper to render json.  This sets the content_type to `application/json`
-  def json(body, status_code = 200)
-    context.response.status_code = status_code
+  macro json(body, status_code = 200)
+    context.response.status_code = {{ status_code }}
     context.response.content_type = "application/json"
-    context.response.print(body)
+    context.response.print({{ body }})
   end
 
   # helper to render xml.  This sets the content_type to `application/xml`
-  def xml(body, status_code = 200)
-    context.response.status_code = status_code
+  macro xml(body, status_code = 200)
+    context.response.status_code = {{ status_code }}
     context.response.content_type = "application/xml"
-    context.response.print(body)
+    context.response.print({{ body }})
   end
 end
